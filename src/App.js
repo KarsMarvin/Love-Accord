438b1c818f7f7061843a238bec4199967de7b01b
NickSingizwa <singizwanick19@gmail.com>
2022-02-07 16:33:09 +0200

    initiating app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import CharsReg from './CharsReg';
+import Notfound from './Notfound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Router>
+        <Routes>
+          <Route path="/get-your-match" exact element={<CharsReg />} />
+          <Route path="*" element={<Notfound />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/CharsReg.js
@@ -0,0 +1,12 @@
+import './CharsReg.css'
+
+function CharsReg(){
+
+    return(
+        <div className='characteristics'>
+            <h1>Love Accord</h1>
+        </div>
+    )
+}
+
+export default CharsReg;
