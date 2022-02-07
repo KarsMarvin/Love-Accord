0e04ff3c628554542da64fd75b47c2ac3c8f4909
NickSingizwa <singizwanick19@gmail.com>
2022-02-07 21:34:55 +0200

    init 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import CharsReg from './CharsReg';
+import CharsRegFinal from './CharsRegFinal';
 import Notfound from './Notfound';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/get-your-match" exact element={<CharsReg />} />
+          <Route path="/happy-for-you" exact element={<CharsRegFinal />} />
           <Route path="*" element={<Notfound />} />
         </Routes>
       </Router>
