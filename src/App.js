b23c7b19e1d202a8d152b0a4fffca65f55ad36c3
karsmarvin808 <kareramarvin14@gmail.com>
2022-02-08 13:40:56 +0200

    user info

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import CharsReg from './match/CharsReg';
-import Searching from './match/Searching';
-import Values from './match/Values';
-import Music from './match/Music';
-import Sports from './match/Sports';
-import Creativity from './match/Creativity';
-import Interests from './match/Interests';
-import Social from './match/Social';
-import Notfound from './Notfound';
+import SignUp from './components/SignUp'
 
 function App() {
   return (
-    <div className='app'>
-      <Router>
-        <Routes>
-          <Route path="/get-your-match" exact element={<Searching />} />
-          <Route path="/xtics" exact element={<CharsReg />} />
-          <Route path="/desc-your-ideal-match" exact element={<Values />} />
-          <Route path="/music" exact element={<Music />} />
-          <Route path="/sports" exact element={<Sports />} />
-          <Route path="/creativity" exact element={<Creativity />} />
-          <Route path="/interests" exact element={<Interests />} />
-          <Route path="/social" exact element={<Social />} />
-          <Route path="*" element={<Notfound />} />
-        </Routes>
-      </Router>
+    <div className="App">
+      <div className='container'>
+      <SignUp />
+      </div>      
+
     </div>
   );
 }
