aceae3f31ece15cd06b3a155dd7666315b6fb978
A. Belard <angebelard@Gmail.com>
2022-02-11 12:21:04 +0200

    Update Results.jsx

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,16 +1,43 @@
-import './Results.css';
+import './Results.scss';
+import {useState} from "react"
+import jwtDecode from 'jwt-decode';
+import Loader from "../components/heartsloader.gif"
 
 function Results(){
-    
-    return(
-        <div className='results'>
-           <h1>Happy for You😍!</h1>
-              <h2>You are a match for:</h2>
+    let decoded
+    let token = localStorage.token
+    const [userMatch, setuserMatch] = useState(undefined)
+    let wait = true
+    if(token){
+        try {
+            decoded = jwtDecode(token)
+        } catch (error) {
+            window.location.href = "/"
+        }
+    }
+    else{
+        window.location.href = "/"
+    }
+    return wait ?
+        (
+            <div className='waitResults'>
+                 <div>
+                    <h5>Holla! We findin' your match... Wait just <span>3 hours</span></h5>
+                    <br />
+                 <img src={Loader} alt="" />
+                 </div>
+            </div>
+        )
+        :
+        (
+            <div className='results'>
+                <h1>Happy for You😍!</h1>
+                <h2>Your match is:</h2>
                 <div className='results-container mt-4'>
                     <h5>hello results</h5>
                 </div>
-        </div>
-    )
+            </div>
+        )
 }
 
 export default Results;
