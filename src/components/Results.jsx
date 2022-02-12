ecd177d3ca3b2fb5e43fd76adcf13f82f3cdbef2
A. Belard <angebelard@Gmail.com>
2022-02-12 17:48:46 +0200

    Update Results.jsx

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,7 +2,7 @@ import './Results.scss';
 // import {useState} from "react"
 // import jwtDecode from 'jwt-decode';
 import Loader from "../components/heartsloader.gif"
-import { useEffect } from 'react';
+import { useEffect , useState} from 'react';
 import axios from 'axios';
 // import axios from 'axios';
 
@@ -16,8 +16,36 @@ function Results(){
     if(!localStorage.token){
         window.location.href = "/"
     }
-    // const [userMatch, setuserMatch] = useState(undefined)
-    let wait = true
+
+    const [userMatch, setuserMatch] = useState(
+        {
+            "interest": {
+              "char": {
+                "skincolor": "brown",
+                "height": "tall"
+              },
+              "music": "hiphop",
+              "sports": "football",
+              "values": "respect",
+              "searching": "relationship",
+              "creativity": "art"
+            },
+            "otherInt": {
+              "social": "ig",
+              "news": "showbizz"
+            },
+            "_id": "6207b91af8d63e1f87a22b19",
+            "fullName": "VERITE",
+            "darassa": "Y2B",
+            "social": "dd",
+            "gender": "F",
+            "movie": "action",
+            "isTaken": true,
+            "__v": 0
+          }
+    )
+
+    let wait = false
     useEffect(() => {
         let config = {
             headers: {
@@ -32,6 +60,31 @@ function Results(){
         .catch(err => console.log(err.response))
       }
       getResults()
+      setuserMatch( {
+        "interest": {
+          "char": {
+            "skincolor": "brown",
+            "height": "tall"
+          },
+          "music": "hiphop",
+          "sports": "football",
+          "values": "respect",
+          "searching": "relationship",
+          "creativity": "art"
+        },
+        "otherInt": {
+          "social": "ig",
+          "news": "showbizz"
+        },
+        "_id": "6207b91af8d63e1f87a22b19",
+        "fullName": "VERITE",
+        "darassa": "Y2B",
+        "social": "dd",
+        "gender": "F",
+        "movie": "action",
+        "isTaken": true,
+        "__v": 0
+      })
     }, [])
     
     return wait ?
@@ -48,9 +101,32 @@ function Results(){
         (
             <div className='results'>
                 <h1>Happy for You😍!</h1>
-                <h2>Your match is:</h2>
+                <h5>Your match is:</h5>
                 <div className='results-container mt-4'>
-                    <h5>hello results</h5>
+                    {userMatch && 
+                    (
+                        <>
+                            <h6>{userMatch.fullName}</h6>
+                            <div>
+                                <p>{userMatch.darassa}</p>
+                                <p><span>IG: </span>{userMatch.social}</p>
+                            </div>
+                            <div>
+                                <h6 className='text-center'>Interests</h6>
+                                <div>
+                                    {Object.keys(userMatch.interest).map((keyname, i) => 
+                                        typeof userMatch.interest[keyname] !== "object" &&
+                                            (
+                                               <div key={i}>
+                                                    <h6>{keyname.toUpperCase()}</h6>
+                                                    <p>{userMatch.interest[keyname].toString()}</p>
+                                               </div>
+                                            )
+                                    )}
+                                </div>
+                            </div>
+                        </>
+                    )}
                 </div>
             </div>
         )
